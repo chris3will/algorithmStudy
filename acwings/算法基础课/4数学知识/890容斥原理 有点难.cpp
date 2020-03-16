b65ff908a289346617cb353973c1261354f53375
chris3will <13838200732@163.com>
2020-03-16 20:04:17 +0800

    daily

--- acwings/算法基础课/4数学知识/890容斥原理 有点难.cpp
+++ acwings/算法基础课/4数学知识/890容斥原理 有点难.cpp
@@ -1,45 +1,63 @@
+//所有质数的并集中的元素的个数
+//核心思想就是容斥原理
+
 #include<iostream>
+#include<cstdio>
+#include<cstring>
 #include<algorithm>
 
 using namespace std;
-
 typedef long long LL;
-
 const int N = 20;
 
-int n,m;
-int p[N];
+
+int p[N]; //存储输入的质数
+
 
 int main()
 {
-    cin>>n>>m;
-    for(int i=0;i<m;i++)cin>>p[i];  //输入所有的质数
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<m;i++)scanf("%d",&p[i]);
 
-    int res = 0; //表示答案，满足条件的整数的个数
-    for(int i=1;i< 1<<m; i++)  //排除都不选的情况
+    int res = 0;  //记录符合条件的整数的个数
+    //开始循环遍历2^m-1个组合情况
+    
+    for(int i=1;i< 1<<m;i++)
     {
-        int t = 1, cnt = 0;  // t 当前所有质数的乘积，cnt代表当前这个选法中包含几个集合
+        //要确定每次选了几个集合，以及该组合中元素的个数，奇数个要加上，偶数个要减去
+        int t=1,cnt=0;
+        
         for(int j=0;j<m;j++)
-            if(i >> j & 1)
-            {//我们选了这个集合
-                cnt ++ ;
+        {
+
+            if(i >> j&1)
+            {
+                //这一位被选了，该想中多并入了一个集合
+                
+
                 if((LL)t*p[j]>n)
-                {//如果范围过大，我们就不管了，大于了n
+                {
                     t=-1;
                     break;
                 }
-                t *= p[j];
+
+                t*=p[j];
+                cnt ++ ;
             }
 
-        if( t!=-1)
-        { //这些质数的乘积是 <= n 的
-            if(cnt%2)res += n/t;  //奇数就加上
-            else res -=n/t; //偶数减去
         }
+        
+        if(t!=-1)
+        {
+            if(cnt%2) res += n/t;
+            else res -= n/t;
+        }
+
+
     }
 
     cout<<res<<endl;
 
     return 0;
 }
-
---
+++ acwings/算法基础课/4数学知识/扩展欧几里得算法.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int exgcd(int a,int b, int &x,int &y)
+{
+    if(!b)
+    {
+        x=1,y=0;
+        return a;
+    }
+    int d = exgcd(b,a%b,y,x);
+    
+    y-=a/b*x;
+    return d;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        int x,y;
+        exgcd(a,b,x,y);
+        printf("%d %d\n",x,y);
+    }
+    
+    
+    return 0;
+}
